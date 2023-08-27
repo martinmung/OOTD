698262230bc065359d92456396d0405ec2a46225
Martin <martinfx21@gmail.com>
2023-08-27 12:02:34 -0700

    Este es el repositorio del ejercicio para elegir tu outfit

---
+++ main.js
@@ -0,0 +1,171 @@
+var head = prompt("¿Vas a salir al aire libre?").toLowerCase()
+var shirt = prompt("¿Vas a ir a una fiesta?").toLowerCase()
+var bottom = prompt("¿Es una salida elegante?").toLowerCase()
+var shoes = prompt("¿Necesitas caminar mucho?").toLowerCase()
+var bag = prompt("¿Necesitas mucho espacio en tu bolso?").toLowerCase()
+
+console.log(head)
+console.log(shirt)
+console.log(bottom)
+console.log(shoes)
+console.log(bag)
+
+if(head == "si" && shirt == "si" && bottom == "si" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd1.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "si" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd2.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "si" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd3.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "si" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd4.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "no" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd5.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "no" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd6.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "no" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd7.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "si" && bottom == "no" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd8.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "si" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd9.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "si" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd10.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "si" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd11.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "si" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd12.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "no" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd13.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "no" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd14.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "no" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd15.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "si" && shirt == "no" && bottom == "no" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd16.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "si" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd17.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "si" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd18.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "si" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd19.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "si" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd20.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "no" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd21.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "no" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd22.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "no" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd23.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "si" && bottom == "no" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd24.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "si" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd25.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "si" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd26.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "si" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd27.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "si" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd28.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "no" && shoes == "si" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd29.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "no" && shoes == "si" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd30.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "no" && shoes == "no" && bag == "si"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd31.jpg' widt='500' height='500'></img>")
+}
+
+if(head == "no" && shirt == "no" && bottom == "no" && shoes == "no" && bag == "no"){
+    document.write("Este es tu OOTD:")
+    document.write("<img src='./images/ootd32.jpg' widt='500' height='500'></img>")
+}
